fcab52b7458427f877adc90075ad2276c86a8090
Oleg Podsechin <oleg.podsechin@ionsquare.com>
2013-04-17 14:24:01 +0300

    exercise #6  prerequisites

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,25 @@
-function MessagesCtrl($scope) {
-  $scope.email = "john@smith.com";
-  $scope.messages = [
-    {time: Date.now(), text: "hello"},
-    {time: Date.now(), text: "world"}
-  ];
-  $scope.verify = function() {
-    return $scope.email.indexOf('@') == -1;
-  }
-}
+angular.module('app', [])
+  .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+  //$locationProvider.html5Mode(true);
+  $routeProvider.when('/messages', {
+    templateUrl:'/views/messages.html',
+    controller:'MessagesCtrl'
+  });
+  $routeProvider.when('/settings', {
+    templateUrl:'/views/settings.html'
+  });
+  $routeProvider.otherwise({
+    redirectTo:'/'
+  });
+}]).controller('MessagesCtrl',
+  ['$scope', function ($scope) {
+    $scope.email = "john@smith.com";
+    $scope.messages = [
+      {time:Date.now(), text:"hello"},
+      {time:Date.now(), text:"world"}
+    ];
+    $scope.verify = function () {
+      return $scope.email.indexOf('@') == -1;
+    }
+  }]);
+
